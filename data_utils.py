ff9f9f19831c1aca8dcc9ffb847e29a7716ef4c8
Hao-Jun Michael Shi <hjmshi@gmail.com>
2019-06-14 13:45:14 -0500

    Fix dataset loader (#3)
    * fixed typos and modified processCriteo function
    
    * clean up data_utils and checked for loops
    
    * simplify load dataset function

--- data_utils.py
+++ data_utils.py
@@ -3,10 +3,15 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 #
-# Description: generate inputs and targets for the dlrm benchmark
+# Description: generate inputs and targets for the DLRM benchmark
+#
 # Utility function(s) to download and pre-process public data sets
 #   - Kaggle Display Advertising Challenge Dataset
 #       (https://labs.criteo.com/2014/09/kaggle-contest-dataset-now-available-academic-use/)
+#
+# After downloading dataset, run:
+#   getKaggleCriteoAdData(datafile="<path-to-train.txt>", o_filename=kaggle_processed.npz")
+#
 # TODO: add support for other data-sets
 
 from __future__ import absolute_import, division, print_function, unicode_literals
@@ -18,17 +23,20 @@ from os import path
 import numpy as np
 import torch
 
-# Converts matrix of unicode strings into distinct integers.
-# Inputs:
-#     mat (np.array): array of unicode strings to convert
-#     convertDicts (list): dictionary for each column
-#     counts (list): number of different categories in each column
-#
-# Outputs:
-#     out (np.array): array of output integers
-#     convertDicts (list): dictionary for each column
-#     counts (list): number of different categories in each column
+
 def convertUStringToDistinctInts(mat, convertDicts, counts):
+    # Converts matrix of unicode strings into distinct integers.
+    #
+    # Inputs:
+    #     mat (np.array): array of unicode strings to convert
+    #     convertDicts (list): dictionary for each column
+    #     counts (list): number of different categories in each column
+    #
+    # Outputs:
+    #     out (np.array): array of output integers
+    #     convertDicts (list): dictionary for each column
+    #     counts (list): number of different categories in each column
+
     # check if convertDicts and counts match correct length of mat
     if len(convertDicts) != mat.shape[1] or len(counts) != mat.shape[1]:
         print("Length of convertDicts or counts does not match input shape")
@@ -50,24 +58,34 @@ def convertUStringToDistinctInts(mat, convertDicts, counts):
 
     return out, convertDicts, counts
 
-# Process Kaggle Display Advertising Challenge Dataset by converting unicode strings
-# in X_cat to integers and converting negative integer values in X_int.
-# Loads data in form "kaggle_day_i.npz" where i is the day.
+
 def processKaggleCriteoAdData(split, d_path):
+    # Process Kaggle Display Advertising Challenge Dataset by converting unicode strings
+    # in X_cat to integers and converting negative integer values in X_int.
+    #
+    # Loads data in the form "kaggle_day_i.npz" where i is the day.
+    #
+    # Inputs:
+    #   split (int): total number of splits in the dataset (typically 7)
+    #   d_path (str): path for kaggle_day_i.npz files
+
     convertDicts = []
     counts = []
 
+    # check if processed file already exists
     idx = 1
-    while idx < split:
-        if path.exists(str(str(d_path) + "kaggle_day_{0}_processed.npz".format(idx))):
+    while idx <= split:
+        if path.exists(str(d_path) + "kaggle_day_{0}_processed.npz".format(idx)):
             idx += 1
         else:
             break
 
-    if idx < (split - 1):
+    # process data if not all files exist
+    if idx < split + 1:
+
         # process data
         for i in range(1, split):
-            with np.load(str(d_path) + "kaggle_day_{0}_processed.npz".format(i)) as data:
+            with np.load(str(d_path) + "kaggle_day_{0}.npz".format(i)) as data:
 
                 X_cat, convertDicts, counts = convertUStringToDistinctInts(
                     data["X_cat"], convertDicts, counts
@@ -86,15 +104,26 @@ def processKaggleCriteoAdData(split, d_path):
 
         np.savez_compressed(str(d_path) + "kaggle_counts.npz", counts=counts)
     else:
-        print("Using exisiting %skaggle_day_*_processed.npz files" % str(d_path))
+        print("Using existing %skaggle_day_*_processed.npz files" % str(d_path))
 
     return
 
 
-# Concatenates different days of Kaggle data and saves.
 def concatKaggleCriteoAdData(split, d_path, o_filename):
-    print ("Concatinating multiple day kaggle data into %s.npz file" % str(d_path + o_filename))
-    for i in range(1, split):
+    # Concatenates different days of Kaggle data and saves.
+    #
+    # Inputs:
+    #   split (int): total number of splits in the dataset (typically 7)
+    #   d_path (str): path for kaggle_day_i.npz files
+    #   o_filename (str): output file name
+    #
+    # Output:
+    #   o_file (str): output file path
+
+    print ("Concatenating multiple day kaggle data into %s.npz file" % str(d_path + o_filename))
+
+    # load and concatenate data
+    for i in range(1, split + 1):
         with np.load(str(d_path) + "kaggle_day_{0}_processed.npz".format(i)) as data:
 
             if i == 1:
@@ -107,14 +136,13 @@ def concatKaggleCriteoAdData(split, d_path, o_filename):
                 X_int = np.concatenate((X_int, data["X_int"]))
                 y = np.concatenate((y, data["y"]))
 
-        print(
-            "Loaded day:", i, "y = 1:", len(y[y == 1]), "y = 0:", len(y[y == 0]))
+        print("Loaded day:", i, "y = 1:", len(y[y == 1]), "y = 0:", len(y[y == 0]))
 
     with np.load(str(d_path) + "kaggle_counts.npz") as data:
 
         counts = data["counts"]
 
-    print("Loaded: counts")
+    print("Loaded counts!")
 
     np.savez_compressed(
         str(d_path) + str(o_filename) + ".npz",
@@ -127,38 +155,39 @@ def concatKaggleCriteoAdData(split, d_path, o_filename):
     return str(d_path) + str(o_filename) + ".npz"
 
 
-# Transforms Kaggle data by applying log transformation on dense features and
-# converting everything to appropriate tensors.
-#
-# Inputs:
-#     X_cat (ndarray): array of integers corresponding to preprocessed
-#                      categorical features
-#     X_int (ndarray): array of integers corresponding to dense features
-#     y (ndarray): array of bool corresponding to labels
-#     split (bool): flag for splitting dataset into training/validation/test
-#                     sets
-#     randomize (str): determines randomization scheme
-#         "none": no randomization
-#         "day": randomizes each day"s data (only works if split = True)
-#         "total": randomizes total dataset
-#     cuda (bool): flag for enabling CUDA and transferring data to GPU
-#
-# Outputs:
-#     if split:
-#         X_cat_train (tensor): sparse features for training set
-#         X_int_train (tensor): dense features for training set
-#         y_train (tensor): labels for training set
-#         X_cat_val (tensor): sparse features for validation set
-#         X_int_val (tensor): dense features for validation set
-#         y_val (tensor): labels for validation set
-#         X_cat_test (tensor): sparse features for test set
-#         X_int_test (tensor): dense features for test set
-#         y_test (tensor): labels for test set
-#     else:
-#         X_cat (tensor): sparse features
-#         X_int (tensor): dense features
-#         y (tensor): label
 def transformCriteoAdData(X_cat, X_int, y, split, randomize, cuda):
+    # Transforms Kaggle data by applying log transformation on dense features and
+    # converting everything to appropriate tensors.
+    #
+    # Inputs:
+    #     X_cat (ndarray): array of integers corresponding to preprocessed
+    #                      categorical features
+    #     X_int (ndarray): array of integers corresponding to dense features
+    #     y (ndarray): array of bool corresponding to labels
+    #     split (bool): flag for splitting dataset into training/validation/test
+    #                     sets
+    #     randomize (str): determines randomization scheme
+    #         "none": no randomization
+    #         "day": randomizes each day"s data (only works if split = True)
+    #         "total": randomizes total dataset
+    #     cuda (bool): flag for enabling CUDA and transferring data to GPU
+    #
+    # Outputs:
+    #     if split:
+    #         X_cat_train (tensor): sparse features for training set
+    #         X_int_train (tensor): dense features for training set
+    #         y_train (tensor): labels for training set
+    #         X_cat_val (tensor): sparse features for validation set
+    #         X_int_val (tensor): dense features for validation set
+    #         y_val (tensor): labels for validation set
+    #         X_cat_test (tensor): sparse features for test set
+    #         X_int_test (tensor): dense features for test set
+    #         y_test (tensor): labels for test set
+    #     else:
+    #         X_cat (tensor): sparse features
+    #         X_int (tensor): dense features
+    #         y (tensor): label
+
     # define initial set of indices
     indices = np.arange(len(y))
 
@@ -261,32 +290,32 @@ def transformCriteoAdData(X_cat, X_int, y, split, randomize, cuda):
         return X_cat, X_int, y
 
 
-# Passes through entire dataset and defines dictionaries for categorical
-# features and determines the number of total categories.
-#
-# Inputs:
-#    num_smaples : number of sample to read/process
-#    datafile : path to downloaded raw data file
-#    o_filename (str): saves results under filename if filename is not ""
-#
-# Outputs:
-#     kaggle_dictionaries (list): list of dictionaries corresponding to each
-#                     categorical feature mapping each feature to an integer
-#     num_kaggle_categories (list): list of total number of categories
-#                     for each categorical feature
-def getKaggleDisplayAdChallengeDataset(num_samples, datafile="", o_filename=""):
+def getKaggleCriteoAdData(datafile="", o_filename=""):
+    # Passes through entire dataset and defines dictionaries for categorical
+    # features and determines the number of total categories.
+    #
+    # Inputs:
+    #    datafile : path to downloaded raw data file
+    #    o_filename (str): saves results under o_filename if filename is not ""
+    #
+    # Output:
+    #   o_file (str): output file path
+
     d_path = "./kaggle_data/"
+
+    # determine if intermediate data path exists
     if path.isdir(str(d_path)):
         print("Saving intermediate data files at %s" % (d_path))
     else:
         os.mkdir(str(d_path))
         print("Created %s for storing intermediate data files" % (d_path))
 
+    # determine if data file exists (train.txt)
     if path.exists(str(datafile)):
         print("Reading data from path=%s" % (str(datafile)))
     else:
         print(
-            "Path of Kaggle Display AdChallenge Dataset is invalid, please download from https://labs.criteo.com/2014/09/kaggle-contest-dataset-now-available-academic-use/"
+            "Path of Kaggle Display Ad Challenge Dataset is invalid; please download from https://labs.criteo.com/2014/09/kaggle-contest-dataset-now-available-academic-use/"
         )
         exit(0)
 
@@ -316,14 +345,15 @@ def getKaggleDisplayAdChallengeDataset(num_samples, datafile="", o_filename=""):
     X_int = np.zeros((num_data_in_split, 13), dtype="i4")
     X_cat = np.zeros((num_data_in_split, 26), dtype="U8")
 
-    count = 0
-    while split < 7:
+    # check if files exist
+    while split <= 7:
         if path.exists(str(str(d_path) + "kaggle_day_{0}.npz".format(split))):
             split += 1
         else:
             split = 1
             break
 
+    count = 0
     if split == 1:
         # load training data
         with open(str(datafile)) as f:
@@ -378,38 +408,33 @@ def getKaggleDisplayAdChallengeDataset(num_samples, datafile="", o_filename=""):
             y=y,
         )
     else:
-        print("Using exisiting %skaggle_day_*.npz files" % str(d_path))
+        print("Using existing %skaggle_day_*.npz files" % str(d_path))
+
     processKaggleCriteoAdData(split, d_path)
     o_file = concatKaggleCriteoAdData(split, d_path, o_filename)
+
     return o_file
 
 
-# Load specified dataset and process into required format
-def loadDataset(dataset, num_samples, df_path="", data=""):
-    if dataset == "kaggle":
-        df_exists = path.exists(str(data))
-        if df_exists:
-            print("Reading from pre-processed data=%s" % (str(data)))
-            file = str(data)
-        else:
-            o_filename = "kaggleAdDisplayChallenge_processed"
-            file = getKaggleDisplayAdChallengeDataset(num_samples, df_path, o_filename)
-    elif dataset == "terabyte":
-        file = "./terbyte_data/tb_processed.npz"
-        df_exists = path.exists(str(file))
-        if df_exists:
-            print("Reading Terabyte data-set processed data from %s" % file)
-        else:
-            raise (
-                ValueError(
-                    "Terabyte data-set processed data file %s does not exist !!" % file
-                )
-            )
+def loadDataset(data_path):
+    # Load specified dataset and process into required format
+    #
+    # Inputs:
+    #   data_path (str): path to dataset
+
+    # check if dataset exists
+    df_exists = path.exists(str(data_path))
+    if df_exists:
+        print("Reading dataset from %s" % data_path)
     else:
-        raise (ValueError("Invalid dataset !!"))
+        raise (
+            ValueError(
+                "Data file %s does not exist!!" % data_path
+            )
+        )
 
     # load and preprocess data
-    with np.load(file) as data:
+    with np.load(data_path) as data:
 
         X_int = data["X_int"]
         X_cat = data["X_cat"]
