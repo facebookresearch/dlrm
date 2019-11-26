45cbb2159882446278c2d7165dce09675f35bd7e
mnaumovfb <mnaumov@fb.com>
2019-11-26 11:55:08 -0800

    Minor adjustment.
    Summary: Minor adjustment.
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- quorem/qr_embedding_bag.py
+++ quorem/qr_embedding_bag.py
@@ -1,3 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
 # Quotient-Remainder Trick
 #
 # Description: Applies quotient remainder-trick to embeddings to reduce
