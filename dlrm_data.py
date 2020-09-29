b016326fce55c589e1077750b8f5e6ff0056ea07
aghaderi@fb.com <aghaderi@fb.com>
2020-09-29 00:15:32 -0700

    copy dlrm_data.py from PARAM-Bench

--- dlrm_data.py
+++ dlrm_data.py
@@ -6,17 +6,12 @@
 # Description: delivering inputs and targets for the dlrm benchmark
 # The inpts and outputs are used according to the following two option(s)
 # 1) random distribution, generated and loaded based on uniform distribution
-# 2) FB distribution, the synthetic data which already generated based on
-#     the distribution of the Facabook data would be loaded.
+# 2) synthetic data, the synthetic pre-generated data would be loaded.
 
 from __future__ import absolute_import, division, print_function, unicode_literals
-
-# numpy
+import sys
 import numpy as np
 from numpy import random as ra
-
-
-# pytorch
 import torch
 from torch.utils.data import Dataset  # , RandomSampler
 
@@ -93,20 +88,6 @@ class RandomDataset(Dataset):
                 self.num_indices_per_lookup,
                 self.num_indices_per_lookup_fixed
             )
-        elif self.data_generation == "synthetic":
-            (X, lS_o, lS_i) = generate_synthetic_input_batch(
-                self.m_den,
-                self.ln_emb,
-                n,
-                self.num_indices_per_lookup,
-                self.num_indices_per_lookup_fixed,
-                self.trace_file,
-                self.enable_padding
-            )
-        else:
-            sys.exit(
-                "ERROR: --data-generation=" + self.data_generation + " is not supported"
-            )
 
         # generate a batch of target (probability of a click)
         T = generate_random_output_batch(n, self.num_targets, self.round_targets)
@@ -156,71 +137,6 @@ def make_random_data_and_loader(args, ln_emb, m_den):
         drop_last=False,  # True
     )
     return train_data, train_loader
-
-
-def generate_random_data(
-    m_den,
-    ln_emb,
-    data_size,
-    num_batches,
-    mini_batch_size,
-    num_indices_per_lookup,
-    num_indices_per_lookup_fixed,
-    num_targets=1,
-    round_targets=False,
-    data_generation="random",
-    trace_file="",
-    enable_padding=False,
-):
-    nbatches = int(np.ceil((data_size * 1.0) / mini_batch_size))
-    if num_batches != 0:
-        nbatches = num_batches
-        data_size = nbatches * mini_batch_size
-    # print("Total number of batches %d" % nbatches)
-
-    # inputs
-    lT = []
-    lX = []
-    lS_offsets = []
-    lS_indices = []
-    for j in range(0, nbatches):
-        # number of data points in a batch
-        n = min(mini_batch_size, data_size - (j * mini_batch_size))
-
-        # generate a batch of dense and sparse features
-        if data_generation == "random":
-            (Xt, lS_emb_offsets, lS_emb_indices) = generate_uniform_input_batch(
-                m_den,
-                ln_emb,
-                n,
-                num_indices_per_lookup,
-                num_indices_per_lookup_fixed
-            )
-        elif data_generation == "synthetic":
-            (Xt, lS_emb_offsets, lS_emb_indices) = generate_synthetic_input_batch(
-                m_den,
-                ln_emb,
-                n,
-                num_indices_per_lookup,
-                num_indices_per_lookup_fixed,
-                trace_file,
-                enable_padding
-            )
-        else:
-            sys.exit(
-                "ERROR: --data-generation=" + data_generation + " is not supported"
-            )
-        # dense feature
-        lX.append(Xt)
-        # sparse feature (sparse indices)
-        lS_offsets.append(lS_emb_offsets)
-        lS_indices.append(lS_emb_indices)
-
-        # generate a batch of target (probability of a click)
-        P = generate_random_output_batch(n, num_targets, round_targets)
-        lT.append(P)
-
-    return (nbatches, lX, lS_offsets, lS_indices, lT)
 
 
 def generate_random_output_batch(n, num_targets, round_targets=False):
@@ -279,22 +195,23 @@ def generate_uniform_input_batch(
 
     return (Xt, lS_emb_offsets, lS_emb_indices)
 
-class SyntheticDataLoader(Dataset):
+
+class SyntheticDataset(Dataset):
 
     def __init__(
         self,
         mini_batch_size,
         nbatches=1,
-        write_data_folder="./synthetic_data/syn_data_bs65536/",
+        synthetic_data_folder="./synthetic_data/syn_data_bs65536/",
     ):
-        self.write_data_folder = write_data_folder
+        self.synthetic_data_folder = synthetic_data_folder
         self.num_batches = nbatches
         self.mini_batch_size = mini_batch_size
 
-        self.X = torch.load(f"{self.write_data_folder}/X_0.pt")
-        self.lS_o = torch.load(f"{self.write_data_folder}/lS_o_0.pt")
-        self.lS_i = torch.load(f"{self.write_data_folder}/lS_i_0.pt")
-        self.T = torch.load(f"{self.write_data_folder}/T_0.pt")
+        self.X = torch.load(f"{self.synthetic_data_folder}/X_0.pt")
+        self.lS_o = torch.load(f"{self.synthetic_data_folder}/lS_o_0.pt")
+        self.lS_i = torch.load(f"{self.synthetic_data_folder}/lS_i_0.pt")
+        self.T = torch.load(f"{self.synthetic_data_folder}/T_0.pt")
         # print('data loader initiated ...')
 
     def __getitem__(self, index):
@@ -322,9 +239,10 @@ class SyntheticDataLoader(Dataset):
 
 def synthetic_data_loader(args, ln_emb, m_den):
 
-    train_data = RandomDataset(
+    train_data = SyntheticDataset(
         args.mini_batch_size,
-        nbatches=args.num_batches
+        nbatches=args.num_batches,
+        synthetic_data_folder=args.synthetic_data_folder,
     )
     train_loader = torch.utils.data.DataLoader(
         train_data,
@@ -340,7 +258,7 @@ def synthetic_data_loader(args, ln_emb, m_den):
 
 def data_loader(args, ln_emb, m_den):
     data_gens = {"random": make_random_data_and_loader,
-                 "fb_synthetic": synthetic_data_loader,
+                 "synthetic": synthetic_data_loader,
     }
     train_data, train_ld = data_gens[args.data_generation](args, ln_emb, m_den)
 
