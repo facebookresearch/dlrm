377cb352659709f5b300e308a32604413ee59c7a
Sami Stephen Wilf <samiw1@meta.com>
2022-11-08 20:09:08 -0800

    Add copyright header to torchrec_dlrm/lr_scheduler.py (#286)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/dlrm/pull/286
    
    Add copyright header
    
    Reviewed By: colin2328
    
    Differential Revision: D41146793
    
    fbshipit-source-id: f1ab776a24c754a1d58d745e4e5d64ca0bd0f1ef

--- torchrec_dlrm/lr_scheduler.py
+++ torchrec_dlrm/lr_scheduler.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 # Copied from https://github.com/facebookresearch/dlrm/blob/mlperf/dlrm_s_pytorch.py
 
 import sys
